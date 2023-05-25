5439764c618c54f85a001b6af59f8655ebaf2f6f
raultejada14 <103843445+raultejada14@users.noreply.github.com>
2023-05-25 16:00:10 -0400

    Add files via upload

---
+++ src/data2.js
@@ -0,0 +1,20 @@
+function Business(image, name, address, city, state, zipcode, category, rating, reviewCount) {
+    const BusinessList = new Map ([
+        
+
+    ]);
+    return (
+        <div>
+
+        </div>
+    /*this.image = image;
+    this.name = name;
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zipcode = zipcode;
+    this.category = category;
+    this.rating = rating;
+    this.reviewCount = reviewCount;*/
+    )
+}
